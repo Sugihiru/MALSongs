11d58c5bff8a5b77e4f0b8d9b4e8b97054e47016
Cedric Khin <cedric.khin@epitech.eu>
2018-08-30 17:14:48 +0200

    Use requests instead of urllib

--- src/html_utils.py
+++ src/html_utils.py
@@ -2,12 +2,10 @@
 # -*- coding: utf-8 -*-
 
 
-import urllib.request
+import requests
 
 
 def get_content_from_url(url):
     """Get the content of the page from the URL as a string"""
-    fp = urllib.request.urlopen(url)
-    bytes_content = fp.read()
-    fp.close()
-    return bytes_content.decode("utf8")
+    r = requests.get(url)
+    return r.text
