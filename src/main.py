2a57a7b639c8df19cc35bb3c0145d1be40a0f047
Cedric Khin <cedric.khin@epitech.eu>
2018-09-05 17:16:38 +0900

    Argparse

--- src/main.py
+++ src/main.py
@@ -1,14 +1,41 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
+import argparse
 
 import animelist
 import anime
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(description="Get the list of"
+                                     " anime OP and ED from either"
+                                     " MAL user list online or"
+                                     " exported XML extracted from MAL.")
+    parser.add_argument("-u", "--username",
+                        help="Username of the owner of the list to scan")
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-x", "--xml",
+                       help="Get data from XML exported MyAnimeList list")
+    group.add_argument("--online",
+                       action="store_true",
+                       help="Get data from MAL website directly."
+                       " May fail if MAL is unaccessible."
+                       " Can only compute 300 animes max because"
+                       " of MAL website architecture")
+    args = parser.parse_args()
+
+    if not args.xml and not args.online:
+        parser.error("Provide at least one method by either using"
+                     " --xml [file] or --online")
+
+    return args
+
+
 def main(username=None):
-    if not username:
+    args = parse_args()
+    if not args.username:
         username = input("Enter MAL username: ")
     user_animelist = animelist.AnimeList(username)
     print("Getting animes from MAL anime list...")
@@ -18,7 +45,4 @@ def main(username=None):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) >= 2:
-        main(sys.argv[1])
-    else:
-        main()
+    main()
