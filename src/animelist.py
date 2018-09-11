aed55460f8b0ee4c299c87754175a49a91e40234
Sugiuro <sugiuro.dev@gmail.com>
2018-09-11 15:59:22 +0900

    Removed --online support

--- src/animelist.py
+++ src/animelist.py
@@ -2,32 +2,18 @@
 # -*- coding: utf-8 -*-
 
 
-import json
-from html.parser import HTMLParser
 from bs4 import BeautifulSoup
 
 from anime import Anime
-import html_utils
-
-MAL_ANIMELIST_BASE_URL = "https://myanimelist.net/animelist/"
 
 
 class AnimeList():
     """Holds info about a user's anime list
     """
 
-    def __init__(self, username, xml_animelist):
-        self.username = username
-        self.url = MAL_ANIMELIST_BASE_URL + username
-
-        if xml_animelist:
-            self.source = 'XML'
-            self.parser = AnimeListXmlParser()
-            self.content = open(xml_animelist, 'r').read()
-        else:
-            self.source = 'HTML'
-            self.parser = AnimeListHtmlParser()
-            self.content = html_utils.get_content_from_url(self.url)
+    def __init__(self, xml_animelist):
+        self.parser = AnimeListXmlParser()
+        self.content = open(xml_animelist, 'r').read()
 
     def get_list_of_animes(self):
         """Returns a list of Anime
@@ -40,22 +26,7 @@ class AnimeList():
         return animes
 
 
-class AnimeListHtmlParser(HTMLParser):
-    def __init__(self):
-        super().__init__()
-        self.anime_data = list()
-
-    def handle_starttag(self, tag, attrs):
-        """Add each anime dict to self.anime_data. Returns None"""
-        if tag == "table":
-            for attr in attrs:
-                if attr[0] == 'data-items':
-                    json_data = json.loads(attr[1])
-                    for anime_user_info in json_data:
-                        self.anime_data.append(anime_user_info)
-
-
-class AnimeListXmlParser(HTMLParser):
+class AnimeListXmlParser():
     def __init__(self):
         super().__init__()
         self.anime_data = list()
--- src/main.py
+++ src/main.py
@@ -9,36 +9,20 @@ import anime
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Get the list of"
-                                     " anime OP and ED from either"
-                                     " MAL user list online or"
+                                     " anime OP and ED from"
                                      " exported XML extracted from MAL.")
-    parser.add_argument("-u", "--username",
-                        help="Username of the owner of the list to scan")
 
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument("-x", "--xml",
-                       help="Get data from XML exported MyAnimeList list")
-    group.add_argument("--online",
-                       action="store_true",
-                       help="Get data from MAL website directly."
-                       " May fail if MAL is unaccessible."
-                       " Can only compute 300 animes max because"
-                       " of MAL website architecture")
+    parser.add_argument("--xml",
+                        required=True,
+                        help="Get data from XML exported MyAnimeList list")
     args = parser.parse_args()
-
-    if not args.xml and not args.online:
-        parser.error("Provide at least one method by either using"
-                     " --xml [file] or --online")
 
     return args
 
 
 def main(username=None):
     args = parse_args()
-    if not args.username:
-        args.username = input("Enter MAL username: ")
-    user_animelist = animelist.AnimeList(args.username,
-                                         xml_animelist=args.xml)
+    user_animelist = animelist.AnimeList(args.xml)
     print("Getting animes from MAL anime list...")
     user_animes = user_animelist.get_list_of_animes()
     print("Animes processed: {0}".format(len(user_animes)))
